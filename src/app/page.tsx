2f3fa8ad8d19c06d3bc12a7fcc462b3af84686c8
davidGBbr <davidgomesbrigido@gmail.com>
2023-07-22 19:45:37 -0300

    structure and style update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import StudentsTable from "@/components/StudentsTable";
+import { students } from "@/data/Student";
 
 const page = () => {
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <StudentsTable />
+    <div className="container mx-auto">
+      <h1 className="text-5xl text-center m-5">Lista de Estudantes</h1>
+      <StudentsTable students={students} />
     </div>
   );
 };
--- src/components/StudentsTable.tsx
+++ src/components/StudentsTable.tsx
@@ -1,46 +1,57 @@
-import { students } from "@/data/Student";
+import { Student } from "@/types/Student";
 
-const StudentsTable = () => {
+interface Props {
+  students: Student[];
+}
+
+const StudentsTable = ({ students }: Props) => {
   const calculateAverage = (grade1: number, grade2: number) => {
     const result = (grade1 + grade2) / 2;
     return result.toFixed(1).toString();
   };
   return (
     <div className="container">
-      <table className="w-full rounded bg-gray-500 table">
-        <thead className="bg-gray-800">
-          <tr className="text-left">
-            <th className="p-2">Name</th>
-            <th>Status</th>
-            <th>Grade 1</th>
-            <th>Grade 2</th>
-            <th>Final Grade</th>
+      <table className="w-full border border-gray-600 rounded-md overflow-hidden">
+        <thead>
+          <tr className="text-left border-b border-gray-600 bg-gray-800">
+            <th className="p-3">Name</th>
+            <th className="p-3">Status</th>
+            <th className="p-3">Grade 1</th>
+            <th className="p-3">Grade 2</th>
+            <th className="p-3">Final Grade</th>
           </tr>
         </thead>
         <tbody>
           {students.map((student) => (
-            <tr className="text-left text-black">
-              <td className="flex items-center gap-2 p-2">
+            <tr
+              key={student.id}
+              className="text-left text-gray-800 bg-gray-400 border-b border-gray-600"
+            >
+              <td className="p-3 flex items-center gap-3">
                 <img
                   className="w-10 h-10 rounded-full"
                   src={student.avatar}
-                  alt={student.avatar + student.name}
+                  alt={student.name}
                 />
                 <div className="flex flex-col">
                   <span className="font-bold">{student.name}</span>
                   <span>{student.email}</span>
                 </div>
               </td>
-              <td
-                className={`text-white w-2 h-2 border  ${
-                  student.active ? "bg-green-600" : "bg-red-600"
-                }`}
-              >
-                {student.active ? "Active" : "Inactive"}
+              <td className="p-3 text-white">
+                {student.active ? (
+                  <div className="px-2 py-1 inline-block rounded-md text-xs border border-green-800 bg-green-600">
+                    Active
+                  </div>
+                ) : (
+                  <div className="px-2 py-1 inline-block rounded-md text-xs border border-red-800 bg-red-600">
+                    Inactive
+                  </div>
+                )}
               </td>
-              <td>{student.grade1}</td>
-              <td>{student.grade2}</td>
-              <td>
+              <td className="p-3">{student.grade1.toFixed(1)}</td>
+              <td className="p-3">{student.grade2.toFixed(1)}</td>
+              <td className="p-3 font-bold">
                 {student.active
                   ? calculateAverage(student.grade1, student.grade2)
                   : "--"}
